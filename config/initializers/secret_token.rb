6a575ff490f556d901eb47d46c4299b907894352
Thomas Copeland <thomas@genyusideas.com>
2013-10-22 20:55:42 -0700

    Adding Repo to GitHub

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Urly::Application.config.secret_token = 'ea80c66e137e40f3c2ce3f0407ab096c28bfdfc742eb6404a64182b9d7e32642882ff04d3e2b5b28a4e2fe33f4349f02451be59820f7b02b9dd1a52e39b36a57'
